e37a84c3af8fed00a7a61b67cd7735514e8a8c22
AITI Student <aiti-student@AITI-87.(none)>
2012-06-19 15:41:46 +0000

    first python coding,it was fun

---
+++ Day1.py
@@ -0,0 +1,8 @@
+print 'Nathaniel Yebuah'
+print "hello, World!!"
+
+a = 95
+print 'the value of a is',a
+
+
+
---
+++ day1.py
@@ -0,0 +1,15 @@
+c=11/2
+d=11.0/2
+print 'the value of 11/2 is',c
+print 'the value of 11.0/2 is',d
+print ''
+e=3**27
+print 'the value of 3 to the power 27 is',e
+print ''
+f=(134533+6889980-84849)**3
+print 'the value of (134533+6889980-84849) to the power 3 is',f
+print''
+g=45/7.9
+print 'the average time per meter is',g,'min/meter.'
+a=((4.7**2+9.4**2)**(1/2))*0.621371
+print 'the shrotest distance btween me and ma friend is',a,'miles.'
